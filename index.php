384955403fae50753d9e1958eeb4d7e0e29c9dfb
raphael zimmer <guidozimmer@mindstream.de>
2024-10-11 16:52:03 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<title>Domain nicht aktiv!</title>
+		<meta charset="utf-8"/>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">
+		<link rel = "stylesheet" type = "text/css" href = "https://www.firestorm.ch/skel/style.css" />
+	</head>
+	<body>
+<div class="top-rect">
+  <a href='https://www.firestorm.ch/' target="_blank"><img src='https://www.firestorm.ch/skel/logo_firestorm.png' alt='FireStorm ISP GmbH'/></a>
+</div>
+		<div class="bottom">
+			<h1>Hier entsteht eine neue Internetpr&auml;senz</h1><br/>
+			<p><br><br>
+			<h3>Ist das deine Domain?</h3><br/>
+			<p>Diese Website hat noch keinen Inhalt. Lade jetzt deine Websitedaten auf deinen Speicherplatz hoch.</p>
+			<p>Oder verwende unser Softwarecenter um Wordpress, Joomla, Typo3 und vieles Mehr zu installieren.</p> 
+			<br/>
+			
+			<p>Solltest du Unterst&uuml;tzung ben&ouml;tigen schaue in unsere <a href='https://www.firestorm.ch/kb/wie-kann-ich-dateien-ueber-ftp-aendern/' target='_blank'>Anleitungen</a> oder <a target="_blank" href='https://www.firestorm.ch/kontakt/'>kontaktiere uns</a>!</p>
+		</div>
+		<div class='footer'>
+			Kontaktiere uns: <a href='https://www.firestorm.ch/support'>FireStorm ISP GmbH</a>
+		</div>
+	</body>
+</html>
